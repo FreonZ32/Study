1ce1bcbadc9bd527f67684e1c134f773bfad70a1
BuildTools <unconfigured@null.spigotmc.org>
2021-07-16 00:19:25 +0500

    Homework

--- Random/Source.cpp
+++ Random/Source.cpp
@@ -10,8 +10,8 @@ using std::srand;
 
 //#define RANDARRAY	//a two-dimensional array with random values
 //#define ARRAYSHOWMANY	//a one-dimensional array from 0 to 10 with the output of the number of repetitions for repeating numbers
-//#define ARRAYSHOWMANY2	//a lot of mistakes, but work
-#define ARRAYSHOWMANY3	//best way
+#define ARRAYSHOWMANY2	//a lot of mistakes, but work (rand%10+100 & (char)arr will broke prg)
+//#define ARRAYSHOWMANY3	//best way
 
 void main()
 {
