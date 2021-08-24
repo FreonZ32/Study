ca2c7b58005395ba46af5e9f693c2af9e91b355f
BuildTools <unconfigured@null.spigotmc.org>
2021-08-24 22:18:52 +0500

    hw

--- Recursion2/Source.cpp
+++ Recursion2/Source.cpp
@@ -12,13 +12,25 @@ void elevator(int floor)
 	elevator(floor-1);
 	cout << floor << endl;
 }
-
+ 
+int factorial(int n)
+{
+	/*if (n == 0)return 1;
+	return  n * factorial(n - 1);*/
+	return n == 0 ? 1 : n * factorial(n - 1);
+}
+double power(int a, int n)
+{
+	if (n == 0)return 1;
+	return a * power(a, n - 1);
+}
+ 
 void main()
 {
 		setlocale(LC_ALL, "rus");
 		/*cout << "Hello world!";
 		main();*/
-		int floor;
-		cout << "¬ведите номер этажа: "; cin >> floor;
-		elevator(floor);
+		int n;
+		cout << "¬ведите число: "; cin >> n;
+		cout << factorial(n);
 }
