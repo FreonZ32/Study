4905cd5ccad8353748c7be431fca6f3a6bbb2531
BuildTools <unconfigured@null.spigotmc.org>
2021-08-26 18:55:53 +0500

    HW

--- Pointers/Source.cpp
+++ Pointers/Source.cpp
@@ -5,7 +5,7 @@ void main()
 {
 	setlocale(LC_ALL, "rus");
 
-	const int X = 5;
+	/*const int X = 5;
 	int a[X] = { 1,2,3,4,5 };
 	int* impostorA;
 	int i = 1;
@@ -15,5 +15,34 @@ void main()
 	*(impostorA + 3) = 5;
 	*(impostorA + 4) = 4;
 	for (int i = 0; i < X; i++)
-		cout << *(impostorA + i) << " ";
+		cout << *(impostorA + i) << " ";*/
+
+	/*int a = 5;
+	int * b = &a;
+	int c = *b;
+	cout << c << " " << b+1 << " " << b;*/
+
+	//int arr[10] = {3,6,9,12,15,18,21,24,27,30};
+
+	//int* a = &arr[0];
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	//cout << arr+i << "=" << *(arr + i) << endl;
+	//	cout << a << " = " << *a << endl;
+	//	a++;
+	//}
+
+	char arr[] = "Hello!";
+	char* a = arr;
+
+	cout << "a = " << a << endl;
+
+	char* a3 = &arr[3];
+	char* a0 = &arr[0];
+
+	cout << "a3 = " << *a3 << endl;
+	cout << "a0 = " << (int)a0 << endl;
+	cout << "diff = " << a3 - a0 << endl;
+	
 }
