60aec28bb1915884c86edda3a2774054cd6fc26f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-17 18:21:25 +0500

    HW

--- DynamicMemory/Source.cpp
+++ DynamicMemory/Source.cpp
@@ -8,13 +8,13 @@ using std::endl;
 void EndAndAgain(int arr[], int& n, int indif, int value, int escape);
 void FillRand(int arr[], int &n);
 void Print(int arr[], int &n);
-int* puch_back(int arr[], int &n, int value);
-int* puch_front(int arr[], int& n, int value);
-int* insert(int arr[], int& n,int indif, int value);
-int* replacement(int arr[], int& n, int indif, int value);
-int* pop_back(int arr[], int& n);
-int* pop_front(int arr[], int& n);
-int* erase(int arr[], int& n, int indif);
+void puch_back(int** arr, int &n, int value);
+void puch_front(int** arr, int& n, int value);
+void insert(int** arr, int& n,int indif, int value);
+void replacement(int** arr, int& n, int indif, int value);
+void pop_back(int** arr, int& n);
+void pop_front(int** arr, int& n);
+void erase(int** arr, int& n, int indif);
 int invalue(int &value);
 int inindif(int& indif);
 void Display(int arr[], int& n, int indif, int value, int escape);
@@ -50,116 +50,98 @@ void Print(int arr[], int &n)
 		cout << arr[i] << "\t";
 	}cout << endl;
 }
-int* puch_back(int arr[], int &n, int value)
+void puch_back(int** arr, int& n, int value)
 {
 	int* buffer = new int[n + 1];
-	for (int i = 0; i < n; i++)buffer[i] = arr[i];
-	delete[] arr;
-	arr = buffer;
-	arr[n] = value;
-	n++;
-	return arr;
+	for (int i = 0; i < n; i++)buffer[i] = (*arr)[i];
+	delete[] * arr;
+	*arr = buffer;
+	(*arr)[n++] = value;
 }
-int* puch_front(int arr[], int& n, int value)
+void puch_front(int** arr, int& n, int value)
 {
 	int* buffer = new int[n + 1];
 	for (int i = 0; i < n; i++)
 	{
-		buffer[i+1] = arr[i];
+		buffer[i+1] = (*arr)[i];
 	}
-	delete[] arr;
-	arr = buffer;
-	arr[0] = value;
+	delete[] *arr;
+	*arr = buffer;
+	(*arr)[0] = value;
 	n++;
-	return arr;
 }
-int* insert(int arr[], int& n, int indif, int value)
+void insert(int** arr, int& n, int indif, int value)
 {
-	if (indif > n)
+	if (indif > n)cout << " The value exceeds the size of the array!\n";
+	else
 	{
-		cout << " The value exceeds the size of the array!\n";
-		return arr;
+		int* buffer = new int[n+1];
+		for (int i = 0; i < n; i++)
+		{
+			if (i >= indif)buffer[i+1] = (*arr)[i];
+			else buffer[i] = (*arr)[i];
+		}
+		delete[] *arr;
+		*arr = buffer;
+		(*arr)[indif] = value;
+		n++;
 	}
-	int* buffer = new int[n + 1];
-	for (int i = 0, k = 0; i < n; i++,k++)
-	{
-		if (i == indif)k++;
-		buffer[k] = arr[i];
-	}
-	delete[] arr;
-	arr = buffer;
-	arr[indif] = value;
-	n++;
-	return arr;
 }
-int* replacement(int arr[], int& n, int indif, int value)
+void replacement(int** arr, int& n, int indif, int value)
 {
-	if (indif > n)
+	if (indif > n) cout << " The value exceeds the size of the array!\n";
+	else
 	{
-		cout << " The value exceeds the size of the array!\n";
-		return arr;
+		int* buffer = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			if (i == indif) { buffer[i] = value; i++; }
+			buffer[i] = (*arr)[i];
+		}
+		delete[] *arr;
+		*arr = buffer;
 	}
-	int* buffer = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		if (i == indif) { buffer[i] = value; i++; }
-		buffer[i] = arr[i];
-	}
-	delete[] arr;
-	arr = buffer;
-	return arr;
 }
-int* pop_back(int arr[], int& n)
+void pop_back(int** arr, int& n)
 {
-	if ((n-1) == 1)
+	if ((n-1) == 0)cout << " Where is less? \n";
+	else
 	{
-		cout << " Where is less? \n";
-		return arr;
+		int* buffer = new int[--n];
+		for (int i = 0; i < n; buffer[i] = (*arr)[i], i++);
+		delete[] *arr;
+		*arr = buffer;
 	}
-	n--;
-	int* buffer = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		buffer[i] = arr[i];
-	}
-	delete[] arr;
-	arr = buffer;
-	return arr;
 }
-int* pop_front(int arr[], int& n)
+void pop_front(int** arr, int& n)
 {
-	if ((n-1) == 1)
+	if((n-1) == 0)cout << " Where is less? \n";
+	else
 	{
-		cout << " Where is less? \n";
-		return arr;
+		int* buffer = new int[n-1];
+		for (int i = 0; i < n-1; buffer[i++] = (*arr)[i + 1]);
+		delete[] *arr;
+		*arr = buffer;
+		n--;
 	}
-	int* buffer = new int[n-1];
-	for (int i = 0; i < n-1; i++)
-	{
-		buffer[i] = arr[i + 1];
-	}
-	delete[] arr;
-	arr = buffer;
-	n--;
-	return arr;
+	
 }
-int* erase(int arr[], int& n, int indif)
+void erase(int** arr, int& n, int indif)
 {
-	if (indif > n)
+	if (indif > n) cout << " The value exceeds the size of the array!\n";
+	else
 	{
-		cout << " The value exceeds the size of the array!\n";
-		return arr;
+		int* buffer = new int[n-1];
+		for (int i = 0; i < n; i++)
+		{
+			if (i >= indif) buffer[i] = (*arr)[i+1];
+			else buffer[i] = (*arr)[i];
+		}
+		delete[] *arr;
+		*arr = buffer;
+		n--;
 	}
-	int* buffer = new int[n-1];
-	for (int i = 0,k = 0; i < n; i++)
-	{
-		if (i == indif)k++;
-		buffer[i] = arr[i+k];
-	}
-	delete[] arr;
-	arr = buffer;
-	n--;
-	return arr;
+
 }
 int invalue(int &value)
 {
@@ -173,21 +155,28 @@ int inindif(int &indif)
 }
 void Display(int arr[], int& n, int indif, int value, int escape)
 {
-	cout << "\n\tWhat do you want to do? \n 1) Insertion in the end of the array \n 2) Insert values in the beginning of the array \n 3) Insertion of the value of the index \n "
-		<< "4) Replacing the value of index \n 5) Deleting the last element of the array \n 6) Deleting the zero element of the array "
-		<<"\n 7) Deleting an array according to the specified index \n 8) Create a new array \n 9) Exit from the program \n>";
+cout	<< "\n\tWhat do you want to do? \n " 
+		<< "1) Insertion in the end of the array \n "
+		<< "2) Insert values in the beginning of the array \n "
+		<< "3) Insertion of the value of the index \n "
+		<< "4) Replacing the value of index \n "
+		<< "5) Deleting the last element of the array \n "
+		<< "6) Deleting the zero element of the array \n "
+		<< "7) Deleting an array according to the specified index \n "
+		<< "8) Create a new array \n "
+		<< "9) Exit from the program \n>";
 	switch (_getch())
 	{
-	case '1':arr = puch_back(arr, n, invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
-	case '2':arr = puch_front(arr, n, invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
-	case '3':arr = insert(arr,n,inindif(indif),invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
-	case '4':arr = replacement(arr, n, inindif(indif), invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
-	case '5':arr = pop_back(arr,n), EndAndAgain(arr, n, indif, value, escape); break;
-	case '6':arr = pop_front(arr,n), EndAndAgain(arr, n, indif, value, escape); break;
-	case '7':arr = erase(arr,n,inindif(indif)), EndAndAgain(arr, n, indif, value, escape); break;
+	case '1':puch_back(&arr, n, invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
+	case '2':puch_front(&arr, n, invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
+	case '3':insert(&arr,n,inindif(indif),invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
+	case '4':replacement(&arr, n, inindif(indif), invalue(value)), EndAndAgain(arr, n, indif, value, escape); break;
+	case '5':pop_back(&arr,n), EndAndAgain(arr, n, indif, value, escape); break;
+	case '6':pop_front(&arr,n), EndAndAgain(arr, n, indif, value, escape); break;
+	case '7':erase(&arr,n,inindif(indif)), EndAndAgain(arr, n, indif, value, escape); break;
 	case '8': delete[] arr, system("pause"), system("cls"), main(); break;
 	case '9': delete[] arr, escape = true; cout << endl << " Exit "; break;
-	default: cout << "Error!", delete[] arr;
+	default: cout << "Incorrect choice!\n", delete[] arr, system("pause"), system("cls"), main();;
 	}
 }
 
