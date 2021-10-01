0dfb6c8844db6a41fdabe66a4d46977167ef199b
BuildTools <unconfigured@null.spigotmc.org>
2021-10-01 13:43:21 +0500

    HW

--- GamePatnashki/Source.cpp
+++ GamePatnashki/Source.cpp
@@ -7,9 +7,13 @@ using std::endl;
 bool Escape = false;
 const int height = 4, length = 4;
 
+
+void Drawing(int**& arr,int height, int length);
+
+
 void main()
 {
-	setlocale(LC_ALL, "rus");
+	//setlocale(LC_ALL, "rus");
 	int** arr = new int* [height];
 	for (int i = 0; i < height; i++)arr[i] = new int [length] {};
 	for (int i = 0; i < height*length; i++)
@@ -23,13 +27,7 @@ void main()
 			}
 		}
 	}
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < length; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}cout << endl;
-	}
+
 	/*do
 	{
 
@@ -37,4 +35,16 @@ void main()
 	} while (Escape);*/
 	for (int i = 0; i < height; i++)delete[] arr[i];
 	delete[] arr;
+}
+
+void Drawing(int**& arr, int height, int length)
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			if (arr[i][j] == 0) cout << char(219) << "\t";
+			else cout << arr[i][j] << "\t";
+		}cout << endl;
+	}
 }
