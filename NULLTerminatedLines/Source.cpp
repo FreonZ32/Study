430750a855f26dc63b98db1d90e33b6d99d50f85
BuildTools <unconfigured@null.spigotmc.org>
2021-10-08 17:47:02 +0500

    HW

--- NULLTerminatedLines/Source.cpp
+++ NULLTerminatedLines/Source.cpp
@@ -5,7 +5,11 @@ using::cin;
 using::cout;
 using::endl;
 
+void ASCII();
 int SringLength(char str[]);
+void to_upper(char* str, int a = ((int)'а'), int z = ((int)'я'), int l = -32);
+void to_lower(char* str);
+void shrink(char* str);
 
 
 void main()
@@ -21,16 +25,46 @@ void main()
 	SetConsoleOutputCP(1251);
 	cin.getline(str, n);	//ввод строк с пробелами
 	cout << str << endl;
-	cout << SringLength(str);
+	to_upper(str);
+	cout << str << endl;
+	to_lower(str);
+	cout << str << endl;
+	shrink(str);
+	cout << str << endl;
+	//cout << SringLength(str);
 
+	//ASCII();
 }
+
+void ASCII()
+{
+	for (int i = 0; i < 256; i++)
+	{
+		cout << i << "\t" << (char)i << endl;
+	}
+}
+
 int SringLength(char str[])
 {
-	int n =0;
-	/*for (int i = 0; str[i]; i++)
-	{
-		if ((int)str[i] != 0)n++;
-	}*/
+	int n = 0;
 	for (; str[n]; n++);
 	return n;
+}
+
+void to_upper(char* str, int a, int z, int l)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (((int)str[i] >= a) && (int)str[i] <= z) str[i] = char((int)str[i] + l);
+	}
+}
+
+void to_lower(char* str)
+{
+	to_upper(str, ((int)'А'), ((int)'Я'), 32);
+}
+
+void shrink(char* str)
+{
+	
 }
--- Poligon/Source.cpp
+++ Poligon/Source.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<string.h>
+#include<stdio.h>
+#include<cstring>
 using namespace std;
 
 void main()
 {
 	setlocale(LC_ALL, "rus");
-
-	char* line;
-	char line2[] = "sssss";
-
-	cout << line;
+	char str2[] = "Hello";
+	int n = 20;
+	char str[20]{};
+	cout << "Input: "; cin >> str;
+	int d = strlen(str);
+	cout << d;
 }
