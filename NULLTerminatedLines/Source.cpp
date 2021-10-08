173de58d7d0c0f83c5c15cffc23d3768fc74e5be
BuildTools <unconfigured@null.spigotmc.org>
2021-10-08 23:33:09 +0500

    HW

--- NULLTerminatedLines/Source.cpp
+++ NULLTerminatedLines/Source.cpp
@@ -12,27 +12,41 @@ void to_lower(char* str);
 void shrink(char* str);
 void is_palindrome(char* str);
 
+void is_int_numer(char* str, char* cout_n, char f, char s, unsigned short& t_o_n, unsigned short n = 2);
+void is_bin_numer(char* str, char* cout_b, char f, char s, unsigned short& t_o_n, unsigned short n = 1);
+void is_hex_numer(char* str, char* cout_h, char f, char s, unsigned short& t_o_n, unsigned short n = 3);
+
+
+char cout_n[] = "Это десятичное ";
+char cout_b[] = "Это двоичное ";
+char cout_h[] = "Это шестнадцетеричное ";
+
 
 void main()
 {
-	setlocale(LC_ALL, "rus");
-	//char str[] = { 'H', 'e','l', 'l', 'o', 0};
-	//char str[] = "Hello";
-	const int n = 50;
-	char str[50]{};
-	cout << " Введите строку: "; 
-	//cin >> str;	//ввод строки (каждый пробел - разделение)
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	cin.getline(str, n);	//ввод строк с пробелами
+	setlocale(LC_ALL, "rus");
+
+	const int n = 50;
+	char str[50]{};
+	unsigned short type_of_number = 0;
+
+	cout << " Введите строку(на русском языке): "; cin.getline(str, n);	//ввод строк с пробелами
+
+	cout << "Переводит строку в верхний регистр: \t"; to_upper(str);
 	cout << str << endl;
-	to_upper(str);
+	cout << "Переводит строку в нижний регистр: \t"; to_lower(str);
 	cout << str << endl;
-	to_lower(str);
+	cout << "Удаляет лишние пробелы из предложения: \t";  shrink(str);
 	cout << str << endl;
-	shrink(str);
-	cout << str << endl;
-	is_palindrome(str);
+	cout << "Проверяет, является ли строка палиндромом: \t";  is_palindrome(str);
+
+	cout << " \nВведите строку с числом )0b(двоичное),1b(десятичное),0x(шестнадцатеричная): "; cin.getline(str, n);
+	is_int_numer(str, cout_n, '1',' b', type_of_number);
+	is_bin_numer(str, cout_b, '0', 'b', type_of_number);
+	is_hex_numer(str, cout_h, '0', 'x', type_of_number);
+	cout << type_of_number;
 
 	//cout << SringLength(str);
 
@@ -46,7 +60,6 @@ void ASCII()
 		cout << i << "\t" << (char)i << endl;
 	}
 }
-
 int SringLength(char str[])
 {
 	int n = 0;
@@ -91,4 +104,31 @@ void is_palindrome(char* str)
 	}
 	if (!palindrom)cout << "Это не палиндром!";
 	else cout << "Это палиндром!";
+}
+
+void is_int_numer(char* str,char* cout_n, char f, char s, unsigned short& t_o_n, unsigned short n)
+{
+	bool int_number = false, integ = false ;
+	if (str[0] == f && str[1] == s)
+	{
+		int_number = true;
+		t_o_n = n;
+		cout << cout_n;
+		int i = 2;
+			while (str[i])
+			{
+				if (str[i] == '.' || str[i] == '/'|| str[i]==',')integ = true;
+				i++;
+			}
+			if (integ) cout << "дробное число!";
+			else cout << "целое число!";
+	}
+}
+void is_bin_numer(char* str,char* cout_b, char f, char s, unsigned short& t_o_n, unsigned short n)
+{
+	is_int_numer(str, cout_b, f, s, t_o_n, n);
+}
+void is_hex_numer(char* str, char* cout_h, char f, char s, unsigned short& t_o_n, unsigned short n)
+{
+	is_int_numer(str, cout_h, f, s, t_o_n, n);
 }
