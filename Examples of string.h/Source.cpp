c3ae0555d5c674f25f10fb8da5b90539cd0ea537
BuildTools <unconfigured@null.spigotmc.org>
2021-10-16 19:59:26 +0500

    HW

--- Examples of string.h/Source.cpp
+++ Examples of string.h/Source.cpp
@@ -2,30 +2,47 @@
 #include<Windows.h>
 #include<string.h>
 using namespace std;
+using::cout;
+using::cin;
+using::endl;
 
 //http://cplusplus.com/reference/cstring/strcspn/
 
 #pragma warning(disable : 4996)		//для s_strcopy 
 
+//Copying//Копирование
 void s_memcpy(char* str_copy, char* str, int num);
 void s_memmove(char* str,int pos1, int pos2, int num);
 void s_strcpy(char* str_copy, char* str);
 void s_strncpy(char* str_copy, char* str, int num);
 
+//Concatenation//Конкатенация
 void s_strcat(char* str, char* str1);
 void s_strncat(char* str, char* str1, int num);
 
+//Comparison//Сравнение
 void s_memcmp(char* str, char* str1, int num);
 void s_strcmp(char* str, char* str1);
 void s_strcoll(char* str, char* str1);
 void s_strncmp(char* str, char* str1, int num);
 void s_strxfrm(char* str1, char* str, int num);		//Странные ошибкb?????????????? когда не закоммичен setlocale
 
+//Searching//Поиск
 void s_memchr(char* str, int value, int num);	//Посмотреть сайт, пункт Portability
 void s_strchr(char* str, int value);
 void s_strcspn(char* str, char* str1);
+void s_strpbrk(char* str, char* str1);
+void s_strrchr(char* str, int value);
+void s_strspn(char* str, char* str1);
+void s_strstr(char* str, char* str1);
+void s_strtok(char* str, char* str1);
 
+//Other
+void s_memset(char* str, int value, int num);
+//void s_strerror(int value);	//Что-то непонятное
+void s_strlen(char* str);
 
+void s_size_t();
 
 void main()
 {
@@ -55,6 +72,14 @@ void main()
 	//s_memchr(str, 'l', 4);
 	//s_strchr(str, 'l');
 	//s_strcspn(str1, str);
+	//s_strpbrk(str, str1);
+	//s_strrchr(str, 'l');
+	//s_strspn(str, str1);
+	//s_strstr(str, str1);
+	//s_strtok(str, str1);
+	//s_memset(str, '-', 3);
+	//s_strlen(str);
+	
 }
 
 void s_memcpy(char* str_copy, char* str, int num)
@@ -134,7 +159,7 @@ void s_strncmp(char* str, char* str1, int num)
 	else cout << "Строки идентичны" << endl;
 }
 void s_strxfrm(char* str1, char* str, int num)
-{	//Преобразует строку str в сооитветствии с текущей локали и копирует num значений в str1, возвращая длинну.
+{	//Преобразует строку str в соответствии с текущей локали и копирует num значений в str1, возвращая длинну.
 	int l;	//Функция возвращает длинну равную колличеству копированных значений (или l == num).
 	l = strxfrm(str1, str, num);
 	l = strxfrm(0, str, 0);
@@ -170,4 +195,74 @@ void s_strcspn(char* str, char* str1)
 	l = strcspn(str, str1);
 	cout << "Символы из " << str1 << " найдены на " << l+1 << " позиции в " << str << endl;
 }
+void s_strpbrk(char* str, char* str1)
+{	//Ищет в строке str первое вхождение символов из строки str1 и возвращает указатель на этот символ.
+	//В случае отсутствия совпадений, функция заканчивает работу на нулевом символе, возвращая указатель на него.
+	char* l;
+	l = strpbrk(str, str1);		//В качестве отправной точки str можно использовать указатель на символ из этого массива.
+	if (l == 0) cout << "Значение не найдено! " << endl;
+	else cout << "Значение " << *l << " найдено на позиции " << (l - str + 1) << " строки " << str << endl;
+	//Интересная модель множественного поиска
+	/*while (l != NULL)
+	{
+		printf("%c ", *l);
+		l = strpbrk(l + 1, str1);
+	}*/
+}
+void s_strrchr(char* str, int value)
+{	//Находит последнее вхождение символа value в строке str и возвращает указатель на него.
+	//Символ null так же проверяется в программе, по этому функцию можно использовать для получения указателя на конец строки.
+	char* l;
+	l = strrchr(str, value);
+	if (l != 0) cout << "Последнее вхождение символа замечено на позиции: " << (l - str + 1) << endl;
+	else cout << "Символ не найден!" << endl;
+}
+void s_strspn(char* str, char* str1)
+{	//Возвращает длинну НАЧАЛЬНОЙ части str, которая состоит только из символов, являющихся частью str1.
+	//null символы функцией не проверяются.
+	int l;
+	l = strspn(str, str1);
+	if (l != 0) cout << "Длинна похожих символов: " << l;
+	else cout << "Совпадений не найдено!" << endl;
+}
+void s_strstr(char* str, char* str1)
+{	//Возвращает указатель на первое совпадение строки (полностью, включая последовательность символов) из str1 в строке str.
+	//Если совпадений не найдено, возвращает null;
+	char* l;
+	l = strstr(str, str1);
+	if (l != 0) cout << "Совпадение найдено на позиции " << (l-str+1) << endl;
+	else cout << "Совпадений не найдено." << endl;
+}
+void s_strtok(char* str, char* str1)
+{	//Последовательный вызов функции выдает указатель на отрезок строки str, разделенный набором символов (либо одним из) из строки str1;
+	//Точка последнего вхождения запоминается самой функцией.
+	//Если функция дошла до 0, то она возвращает указатель на null.
+	char* l;
+	l = strtok(str, str1);
+	cout << "Фразу можно разбить на: " << endl;
+	while (l != 0)
+	{
+		cout << l << endl;
+		l = strtok(0, str1);	//При дальнейших запусках, программа ожидает 0 на указателе, и использует сохраненную в памяти точку конца предыдущего отрезка.
+	}
+}
+void s_memset(char* str, int value, int num)
+{	//Заполняет первые num байт str символом value с заменой начала строки str;
+	//Функция преобразует значение value в unsigned char!
+	memset(str, value, num);
+	cout << "Добвленное значение в начало: " << str << endl;
+}
+void s_strlen(char* str)
+{	//Возвращает длинну строки, заполненную какими либо значениями (не путать с длинной строки при инициализации).
+	printf("Длинна строки равна %u.\n", (unsigned)strlen(str));
+}
 
+void s_size_t()
+{
+	//Псевдоним одного из основных беззнаковых целочисленных типов.
+	//Это тип, способный представлять размер любого объекта в байтах : size_t - это тип, возвращаемый оператором sizeof,
+	//и широко используется в стандартной библиотеке для представления размеров и подсчетов.
+	//В <cstring> он используется в качестве типа параметра num в функциях memchr, memcmp, memcpy, memmove, memset, strncat, strncmp,
+	//strncpy и strxfrm, которые во всех случаях используются для указания максимального количества байтов или символов, на которые должна воздействовать функция.
+	//Он также используется в качестве возвращаемого типа для strcspn, strlen, strspn и strxfrm для возврата размеров и длин.
+}
